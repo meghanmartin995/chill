dffa8e6e5e413d4dceabe647b551d73175394f00
Meghan Martin <meghanxmartin@gmail.com>
2020-05-07 20:00:46 +0200

    creates new topics

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :topics, only: [ :index, :show, ] do
+  resources :topics, only: [ :index, :show, :create ] do
     resources :resources, only: :create
     resources :todos, only: :create
   end
