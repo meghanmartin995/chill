2a8db093d88bd217d1155061dda9fe8c8bdc0aa5
Meghan Martin <meghanxmartin@gmail.com>
2020-05-08 20:56:41 +0200

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
+puts 'Creating users...'
+
+user_1 = User.create!(name: "Meghan", email: "meghanxmartin@gmail.com", password: "123456")
+
+
 puts 'Creating topics...'
 Topic.create!({
-  title: "Javascript"
+  title: "Javascript",
+  color: "A094FF",
+  user: user_1
 })
 Topic.create!({
-  title: "Accessibility"
+  title: "Accessibility",
+  color: "#FFBFFF",
+  user: user_1
 })
 puts 'Finished!'
