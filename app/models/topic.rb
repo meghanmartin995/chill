93d3cdf5c6c0b0755ddd0b1ac5839caaecc80043
Meghan Martin <meghanxmartin@gmail.com>
2020-05-08 19:08:31 +0200

    adds color

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,6 @@ class Topic < ApplicationRecord
   has_many :todos, dependent: :destroy
   has_many :resources, dependent: :destroy
   validates :title, presence: true
+  validates :color, presence: true
   belongs_to :user
 end
