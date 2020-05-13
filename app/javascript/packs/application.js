050597d87f3b12ff74e2e638e3ddf4c2468ba659
Meghan Martin <meghanxmartin@gmail.com>
2020-05-13 19:56:05 +0200

    styles todo page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
+
 //= require bootstrap-modal
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
