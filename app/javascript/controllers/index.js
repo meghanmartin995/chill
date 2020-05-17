16677e067b820f0314487ee241861bb22392e88d
Meghan Martin <meghanxmartin@gmail.com>
2020-05-17 18:31:57 +0200

    removes dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,7 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-import { dropdown } from "packs/dropdown"
+// import { dropdown } from "packs/dropdown"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
